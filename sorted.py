813573d0367eb91a1bb5648d5c21b4b069f241fc
Harikrish456 <64851856+Harikrish456@users.noreply.github.com>
2022-08-15 16:12:55 +0530

    Add files via upload

--- sorted.py
+++ sorted.py
@@ -1,30 +1,20 @@
-import pandas as pd 
-import csv 
+import pandas as pd
 
-df = pd.read_csv('dwarf_stars.csv')
-df = df[df['Mass'].notna()]
-df = df[df['Radius'].notna()]
-headers = ['Star_name', 'Distance', 'Mass', 'Radius']
-##print(df.head(10))
+df = pd.read_csv("dwarf_stars.csv")
+df.columns
+df.dtypes
 
-#Converting each value under mass to a floating point
-tempData_mass = []
-df_mass = df['Mass']
-for i in df_mass:
-    i = float(i)
-    revised_i = i * 0.000954588
-    tempData_mass.append(revised_i)
-        
-##print(tempData_mass)
+df = df.dropna()
+df.dtypes
 
-#Changing radius values
-tempData_radius = []
-df_radius = df['Radius']
-for i in df_radius:
-    i = i * 0.102763
-    tempData_radius.append(i)
-
-print(tempData_radius)
-
-
+df["Radius"] = 0.102763*df["Radius"]
+df['Mass']=df['Mass'].apply(lambda x: x.replace('$', '').replace(',', '')).astype('float')
+df["Mass"] = 0.000954588*df["Mass"]
+df.head()
+df.columns
+df.drop(['Unnamed: 0'],axis=1,inplace=True)
+df.head()
+df.reset_index(drop=True,inplace=True)
+df.to_csv("unit_converted_stars.csv")
+df.dtypes
 
